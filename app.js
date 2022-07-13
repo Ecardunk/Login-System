6e335e9df9ca86a5e7efbbf696eef465cacafc4b
Estevão Cardoso <cardosoestevao@yahoo.com.br>
2022-07-13 19:53:28 -0300

    conexao bd

---
+++ app.js
@@ -0,0 +1,28 @@
+// IMPORTS
+const express = require('express');
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+//
+
+
+//Banco de Dados - Provisório para testes
+const dbUser = process.env.DB_USER
+const dbPassword = process.env.DB_PASSWORD
+mongoose.connect(`mongodb+srv://${dbUser}:${dbPassword}@cluster0.7la1x.mongodb.net/?retryWrites=true&w=majority`).then(() => {
+    console.log("MongoDB Conetado com sucesso!")
+}).catch((err) => {
+    console.log("Houve um erro: " + err)
+});
+//
+
+const app = express();
+
+
+
+// Servidor
+app.listen(3000, () => {
+    console.log('Servidor ON!');
+})

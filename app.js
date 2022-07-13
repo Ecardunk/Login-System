b33a9e1707790c83d54e3593e750adb9d1346d0f
Estevão Cardoso <cardosoestevao@yahoo.com.br>
2022-07-13 20:20:57 -0300

    model adicionado

--- app.js
+++ app.js
@@ -1,24 +1,24 @@
 // IMPORTS
 const express = require('express');
 require('dotenv').config();
-const express = require('express');
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 //
 
+const app = express();
 
 //Banco de Dados - Provisório para testes
 const dbUser = process.env.DB_USER
 const dbPassword = process.env.DB_PASSWORD
 mongoose.connect(`mongodb+srv://${dbUser}:${dbPassword}@cluster0.7la1x.mongodb.net/?retryWrites=true&w=majority`).then(() => {
-    console.log("MongoDB Conetado com sucesso!")
+    console.log("MongoDB Conectado com sucesso!")
 }).catch((err) => {
     console.log("Houve um erro: " + err)
 });
 //
 
-const app = express();
+
 
 
 

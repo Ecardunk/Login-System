6140991a1788b4ad5477a24bc4c7d98dfbb07c20
Matheus Rocha <01devmath@gmail.com>
2022-07-13 21:07:57 -0300

    Adicionar login do administrador

--- app.js
+++ app.js
@@ -9,15 +9,15 @@ const User = require('./models/User');
 
 const app = express();
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 
 //Banco de Dados - Provisório para testes
 const dbUser = process.env.DB_USER
 const dbPassword = process.env.DB_PASSWORD
 mongoose.connect(`mongodb+srv://${dbUser}:${dbPassword}@cluster0.7la1x.mongodb.net/?retryWrites=true&w=majority`).then(() => {
-    console.log("MongoDB Conectado com sucesso!")
+  console.log("MongoDB Conectado com sucesso!")
 }).catch((err) => {
-    console.log("Houve um erro: " + err)
+  console.log("Houve um erro: " + err)
 });
 //
 
@@ -26,25 +26,24 @@ app.get('/', (_req, res) => {
   res.sendFile(__dirname + '/view/index.html');
 });
 
-app.post('/login', async(req, res) => {
+app.post('/login', async (req, res) => {
   const htmlEmail = req.body.email;
   const htmlPassword = req.body.password;
-  
-  const salt = await bcrypt.genSalt(12);
-  const encryptedPassword = await bcrypt.hash(htmlPassword, salt);
 
+  const user = await User.findOne({ email: htmlEmail });
+  if (!user) {
+    return res.status(404).json({msg: 'Usuário não encontrado!'});
+  }
 
-  const user = new User({
-    email: htmlEmail,
-    password: encryptedPassword,
-  });
+  const checkPassword = await bcrypt.compare(htmlPassword, user.password);
+  if (!checkPassword) {
+    return res.status(422).json({msg: 'Senha incorreta!'});
+  }
 
-  await user.save();
-
-
+  return res.send({msg: 'Logado com sucesso!'});
 });
 
 // Servidor
 app.listen(3000, () => {
-    console.log('Servidor ON!');
+  console.log('Servidor ON!');
 })
